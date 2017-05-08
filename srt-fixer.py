068e7929dcfbea339b10388ec1324806339ff4fa
GiChazap <gichazap@gmail.com>
2017-05-09 00:35:58 +0300

    Update srt-fixer.py

--- srt-fixer.py
+++ srt-fixer.py
@@ -4,7 +4,7 @@ import re
 import argparse
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser( description = 'Offset srt subtitles.')
 # add mandatory (positional) arguments
 parser.add_argument("fname",help="input srt file name")
 parser.add_argument("offset",type=float,help="subtitle offset in seconds to apply (can be fractional)")
@@ -14,10 +14,16 @@ args = parser.parse_args()
 
 with open(args.fname,newline='') as ifp:	
 	for line in ifp:
-	
-		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
+		match = re.search(r'^(\d+:\d+:\d+,\d+)\s+--\>\s+(\d+:\d+:\d+,\d+)', line)
+                if match:
+                    sys.write("%s --> %s\n" % (
+                        offset_time(options.offset, match.group(1)),
+                        offset_time(options.offset, match.group(2))
+		    )
+else:
+		
+	       	sys.stdout.write(line)
 
-		sys.stdout.write(line)
-
-		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
+		//`if options.overwrite:
+		shutil.move(out_filename, options.srt_file.name)
 
